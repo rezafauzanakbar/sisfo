0752d2966586ec4e519b12baf603ef4097d543de
Reza Fauzan Akbar <rezafauzanakbar3@gmail.com>
2023-09-19 09:39:27 +0700

    first commit

---
+++ src/main/java/com/jatiluhur/sisfo/SisfoApplication.java
@@ -0,0 +1,13 @@
+package com.jatiluhur.sisfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jatiluhur/sisfo/SisfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jatiluhur.sisfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
