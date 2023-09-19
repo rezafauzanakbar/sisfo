4f2dfda1acbe7e0979330cd73e1eb3d9b43e5766
wahyu-ramadhani69 <wahyu.ramadhani6969@gmail.com>
2023-09-19 10:52:18 +0700

    "wahyu "

--- src/main/java/com/jatiluhur/sisfo/model/Berita.java
+++ src/main/java/com/jatiluhur/sisfo/model/Berita.java
@@ -1,6 +1,13 @@
 package com.jatiluhur.sisfo.model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 
+@Entity
+@Table(name = "Berita")
 public class Berita {
-    
+    @Id
+    private Long id;
+
 }
