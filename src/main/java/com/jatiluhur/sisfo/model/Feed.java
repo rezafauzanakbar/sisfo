547b9f183cb709cf2b7aa7920f584e11c05b4dfe
Emirat Millenium Try <emirat.millenium@yahoo.co.id>
2023-09-19 10:56:23 +0700

    ignore application properties

---
+++ src/main/java/com/jatiluhur/sisfo/model/Feed.java
@@ -0,0 +1,13 @@
+package com.jatiluhur.sisfo.model;
+/*
+IntelliJ IDEA 2023.2 (Community Edition)
+Build #IC-232.8660.185, built on July 26, 2023
+@Author EmiratZ a.k.a. M Emirat Millenium Try
+Java Developer
+Created on 19/09/2023 10.53
+@Last Modified 19/09/2023 10.53
+Version 1.0
+*/
+
+public class Feed {
+}
